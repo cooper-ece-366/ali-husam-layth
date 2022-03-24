5a252f0b7363da1ab201263f1141b171188596e9
husam-6 <husam@almanakly.com>
2022-03-24 18:25:16 -0400

    Change to class component for Restaurant and Home pages

--- camel-app/src/pages/restaurants.js
+++ camel-app/src/pages/restaurants.js
@@ -1,51 +1,64 @@
 import React, {useState, useEffect} from "react";
 import { EventEmitter } from "events";
 import "../App.css"
+import { BASE_URL } from '../constants';
 
+class Restaurants extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+          items: []
+        };
+    }
 
-const Restaurants = () => {
-    const [items, setState] = useState([])
-    const fetchData = () => {
-        var url = "http://localhost:8080/api/restaurants";
+    fetchData(){
+        var url = BASE_URL + "/api/restaurants";
         fetch(url)
               .then(response => response.json())
               .then(data => {
-                setState(data);
-//                console.log(data);
-        }).catch(err => {
+//                setState(data);
+                this.setState({
+                    items : data,
+                });
+                console.log(this.state.items.results)
+                }).catch(err => {
             console.log("Cannot connect to API endpoint: %s", url);
         });
     };
-    useEffect(() => {
-        fetchData();
-      }, [])
+
+    componentDidMount() {
+        this.fetchData();
+     }
+
+
 //    console.log(items.results)
-//    var items = [{"name": "test1", "rating": 4.5, "vicinity": "103 Cock Lane" }, {"name": "test2", "rating": 2.5, "vicinity": "200 layth butt" }, {"name": "test3", "rating": 3.2, "vicinity": "100 Ari Butt" }];
-  return (
-    <div className="App-header">
-      <h1>
-        Restaurants In Your Area
-      </h1>
-      <table className="table table-striped table-bordered">
-          <thead>
-              <tr>
-                  <th>Name</th>
-                  <th>Address</th>
-                  <th>Rating</th>
-              </tr>
-          </thead>
-          <tbody>
-              {items.results && items.results.map(place =>
-                  <tr key={place.id}>
-                      <td>{place.name}</td>
-                      <td>{place.vicinity}</td>
-                      <td>{place.rating}</td>
+  render(){
+      return (
+        <div className="App-header">
+          <h1>
+            Restaurants In Your Area
+          </h1>
+          <table className="table table-striped table-bordered">
+              <thead>
+                  <tr>
+                      <th>Name</th>
+                      <th>Address</th>
+                      <th>Rating</th>
                   </tr>
-              )}
-          </tbody>
-      </table>
-    </div>
-  );
-};
+              </thead>
+              <tbody>
+                  {this.state.items.results && this.state.items.results.map(place =>
+                      <tr key={place.place_id}>
+                          <td>{place.name}</td>
+                          <td>{place.vicinity}</td>
+                          <td>{place.rating}</td>
+                      </tr>
+                  )}
+              </tbody>
+          </table>
+        </div>
+      );
+    };
+  }
 
 export default Restaurants;
