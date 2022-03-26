71b1c36cc887271b1b1d8b27aeefbb6ef6bd661c
husam-6 <husam@almanakly.com>
2022-03-26 15:30:02 -0400

    Fix api

--- build.sh
+++ build.sh
@@ -1,7 +1,66 @@
+# #!/bin/bash
+
+# # TODO: Add in the application you will write below. That will be a folder under this current working directory
+# APPS=""
+
+# # First ensure dependencies loaded since .m2 may be empty
+# mvn dependency:tree -Ddetail=true
+# mvn help:evaluate -Dexpression=project.version
+
+# # Clean repo from builds
+# ./clean.sh
+
+# #
+# # Config
+# #
+# APP_VERSION_FILE=./app.version
+
+# THEUSER=$(/usr/bin/whoami)
+# NOW=$(date "+%Y%m%d%H%M%S")
+# APP_MAVEN_VERSION=$(mvn help:evaluate -Dexpression=project.version | grep -e '^[^\[]')
+# APP_GIT_VERSION=$(git rev-parse --abbrev-ref HEAD)
+
+# APP_VERSION=${APP_MAVEN_VERSION}-${APP_GIT_VERSION}_${NOW}_${THEUSER}
+# echo ${APP_VERSION} > ${APP_VERSION_FILE}
+
+# echo "LOCALLY building runtime to local folder: ./build ..."
+# echo "Version = ${APP_VERSION}"
+
+# if [ ! -f "${APP_VERSION_FILE}" ]; then
+#     echo "APP Version file DOES NOT exist. CANNOT proceed with build."
+#     exit 1
+# fi
+
+# #
+# # Config
+# #
+# APP_VERSION=$(cat ${APP_VERSION_FILE})
+
+# #mvn test
+# mvn install -Dmaven.test.skip=true
+# mvn package -N -P classpath-deps -Dmaven.test.skip=true
+
+# # Package runtimes with compiled and built libraries
+# for APP in $APPS
+# do
+# 	echo "Building $APP ..."
+# 	cd ./$APP
+# 	./build_app.sh
+# 	echo "Built $APP"
+# 	cd ..
+# done
+
+# #echo "Copying job scripts for at scheduling to build/app/ ..."
+# #cp ./cicd/deploy.sh ./build/app/
+# #cp ./app/opt/start_app.sh ./build/app/
+# #echo "Done."
+
+# echo "Done building APP."
+
 #!/bin/bash
 
 # TODO: Add in the application you will write below. That will be a folder under this current working directory
-APPS=""
+APPS="server"
 
 # First ensure dependencies loaded since .m2 may be empty
 mvn dependency:tree -Ddetail=true
@@ -13,15 +72,22 @@ mvn help:evaluate -Dexpression=project.version
 #
 # Config
 #
-APP_VERSION_FILE=./app.version
-
 THEUSER=$(/usr/bin/whoami)
 NOW=$(date "+%Y%m%d%H%M%S")
 APP_MAVEN_VERSION=$(mvn help:evaluate -Dexpression=project.version | grep -e '^[^\[]')
 APP_GIT_VERSION=$(git rev-parse --abbrev-ref HEAD)
 
+RESOURCES_DIR=./server/src/main/resources
+APP_PROP_FILE=${RESOURCES_DIR}/application.properties
+APP_TEMPLATE_PROP_FILE=${RESOURCES_DIR}/application.properties-template
+APP_VERSION_FILE=./app.version
+
 APP_VERSION=${APP_MAVEN_VERSION}-${APP_GIT_VERSION}_${NOW}_${THEUSER}
 echo ${APP_VERSION} > ${APP_VERSION_FILE}
+
+[ -e ${APP_PROP_FILE} ] && rm ${APP_PROP_FILE}
+cat ${APP_TEMPLATE_PROP_FILE} | sed -e "s/FULLAPPBUILDNUM/${APP_VERSION}/g" \
+  | sed -e "s/MVNVERSION/${APP_MAVEN_VERSION}/g" > ${APP_PROP_FILE}
 
 echo "LOCALLY building runtime to local folder: ./build ..."
 echo "Version = ${APP_VERSION}"
@@ -41,14 +107,14 @@ mvn install -Dmaven.test.skip=true
 mvn package -N -P classpath-deps -Dmaven.test.skip=true
 
 # Package runtimes with compiled and built libraries
-for APP in $APPS
-do
-	echo "Building $APP ..."
-	cd ./$APP
-	./build_app.sh
-	echo "Built $APP"
-	cd ..
-done
+#for APP in $APPS
+#do
+#	echo "Building $APP ..."
+#	cd ./$APP
+#	./build_app.sh
+#	echo "Built $APP"
+#	cd ..
+#done
 
 #echo "Copying job scripts for at scheduling to build/app/ ..."
 #cp ./cicd/deploy.sh ./build/app/
---
+++ server/src/main/java/edu/cooper/ece366/project/server/Components/AuthProvider.java
@@ -0,0 +1,8 @@
+package edu.cooper.ece366.project.server.Components;
+
+public enum  AuthProvider {
+    local,
+    facebook,
+    google,
+    github
+}
