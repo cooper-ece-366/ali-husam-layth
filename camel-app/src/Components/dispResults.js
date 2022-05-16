635255e1401b8a9d6c49140b31bd3be9f40d85c7
Ali Ghuman <alighuman50@gmail.com>
2022-05-15 20:08:09 -0400

    initial commit

--- camel-app/src/Components/dispResults.js
+++ camel-app/src/Components/dispResults.js
@@ -3,18 +3,24 @@ import { onSubmit } from "../utils/apiCalls";
 import ReactLoading from "react-loading";
 import Item from "../Components/items.js";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGrinTongueSquint, faSearch } from "@fortawesome/free-solid-svg-icons";
+import {
+  faGrinTongueSquint,
+  faSearch,
+} from "@fortawesome/free-solid-svg-icons";
 import Alert from "react-s-alert";
 import "react-s-alert/dist/s-alert-default.css";
 import "react-s-alert/dist/s-alert-css-effects/slide.css";
 
 import "../pages/items.css";
 
-
 const Display = (props) => {
   return (
     <>
-    <form onSubmit={onSubmit} className="search-box">
+      <div className="button-container">
+        <button className="next-button" onClick={props.prevPage}>
+          Previous
+        </button>
+        <form onSubmit={onSubmit} className="search-box">
           <button className="btn-search" onClick={props.getCoords}>
             <FontAwesomeIcon icon={faSearch} />
           </button>
@@ -26,42 +32,55 @@ const Display = (props) => {
             value={props.city}
           ></input>
         </form>
-        <div className="button-container">
-          <button className="next-button" onClick={()=>{window.location.reload();}}>Reset</button>
-          <button className="next-button" onClick={props.prevPage}>Previous</button>
-          <button className="next-button" onClick={props.nextPage}>Next</button>
-        </div>
+        <button className="next-button" onClick={props.nextPage}>
+          Next
+        </button>
+      </div>
 
-        {(!props.items.length && Array.isArray(props.items)) ||
-        props.items.status === "INVALID_REQUEST" ? (
-          <ReactLoading
-            className="loading"
-            type={"spin"}
-            color={"#92400e"}
-            height={100}
-            width={100}
-          />
-        ) : (
-          <div>
-            <ul className="item-container">
-              {props.items.results &&
-                props.items.results.map((place) => (
-                  <Item
-                    key={place.place_id}
-                    place_name={place.name}
-                    photo_reference={place.photos[0].photo_reference}
-                    vicinity={place.vicinity}
-                    rating={place.rating}
-                    website={place.website}
-                    hours={place.hours}
-                  />
-                  ))}
-            </ul>
-            <Alert stack={{limit: 3}} 
-              timeout = {3000}
-              position='top-right' effect='slide' offset={65} /> 
+      {(!props.items.length && Array.isArray(props.items)) ||
+      props.items.status === "INVALID_REQUEST" ? (
+        <ReactLoading
+          className="loading"
+          type={"spin"}
+          color={"#92400e"}
+          height={100}
+          width={100}
+        />
+      ) : (
+        <div>
+          <ul className="item-container">
+            {props.items.results &&
+              props.items.results.map((place) => (
+                <Item
+                  key={place.place_id}
+                  place_name={place.name}
+                  photo_reference={place.photos[0].photo_reference}
+                  vicinity={place.vicinity}
+                  rating={place.rating}
+                  website={place.website}
+                  hours={place.hours}
+                />
+              ))}
+          </ul>
+          <div class="reset-div">
+            <button
+              className="next-button reset"
+              onClick={() => {
+                window.location.reload();
+              }}
+            >
+              Reset
+            </button>
           </div>
-        )}        
+          <Alert
+            stack={{ limit: 3 }}
+            timeout={3000}
+            position="top-right"
+            effect="slide"
+            offset={65}
+          />
+        </div>
+      )}
     </>
   );
 };
